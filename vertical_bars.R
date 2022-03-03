b53b278f59bbb52d1b45e3961e35bd7792967514
jpmckeown <j.p.mckeown@icloud.com>
2022-03-03 19:04:49 +0000

    problem with green plot

--- vertical_bars.R
+++ vertical_bars.R
@@ -37,6 +37,11 @@ show_col(midBlues5)
 #assemble Biosphere blues
 show_col(biosphereBlues)
 
+GreenLong <- colorRampPalette(brewer.pal(9, 'Greens'))(16)
+midGreens8 <- GreenLong[4:11]
+biosphereBlues <- c("#D8E7F5", "#C9DDF0", "#C9DDF0", "#B3D3E8", 
+                    "#9AC7E0", "#9AC7E0", "#9AC7E0", "#7AB6D9")
+  
 ## Data
 
 original_xls <- "data/DATA EXTRACTION FINAL (16).xlsx"
@@ -59,6 +64,8 @@ thisTable <- tabyl(thisData$Country)
 names(thisTable)[1] <- 'Country'
 
 thisTable <- thisTable[order(thisTable$n),]
+country_table <- thisTable # store
+thisTable <- country_table
 
 Name <- thisTable$Country
 Value <- thisTable$percent
@@ -74,6 +81,7 @@ df <-  data.frame(Name, Absolute, Value, yPos, Str) %>%
   mutate(Name = factor(Name, levels = Name)) 
 
 country_plot_df <- df # store
+df <-  country_plot_df
 
 thisPlot <- ggplot(data = df, aes(x = 1, y = Absolute, fill = Name)) +
   geom_col(color = 'black', size = 0.2) +
@@ -120,3 +128,69 @@ biosphereBycountryData <- tmp %>%
 
 biosphereBycountryData
 
+biosphereFreq <- tabyl(biosphereBycountryData$Biosphere)
+names(biosphereFreq)[1] <- 'Biosphere'
+
+biosphereFreqA <- biosphereFreq %>% 
+  arrange(match(Biosphere, c('Ranong', 'Lore Lindu', 'Komodo', 'Tonle Sap', 'Red River Delta', 'Cu Lao Cham', 'Cat Tien', 'Palawan')))
+thisTable <- biosphereFreqA
+
+Name <- thisTable$Biosphere
+Value <- thisTable$percent
+Absolute <- thisTable$n
+yPos <- cumsum(lag(Value, default = 0))
+percentage <- (round(Value * 100, digits=0))
+Str <- paste0('(', Absolute, ')')
+n <- sum(thisTable$n)
+
+# factor to keep order
+df <-  data.frame(Name, Absolute, Value, yPos, Str) %>%
+  mutate(Name = factor(Name, levels = Name)) 
+
+biosphere_plot_df <- df # store
+df <-  biosphere_plot_df
+
+thisPlot <- ggplot(data = df, aes(x = 1, y = Absolute, fill = Name)) +
+  geom_col(color = 'black', size = 0.2) +
+  geom_text(aes(label = paste(Name, Str)),
+            position = position_stack(), 
+            size = ifelse(Absolute == 1, 5.5, 6.4),
+            vjust = ifelse(Absolute == 1, 1.8, 2.2)) +
+  scale_y_continuous(limits=c(0, n), expand = c(0, 0)) +
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_fill_manual(values = midGreens8) +
+  coord_cartesian(clip = 'off') +
+  barOnlyTheme
+#   scale_fill_manual(values = midGreens8) +
+# scale_fill_manual(values = biosphereBlues) +
+thisPlot
+biosphereVerticalPlotGreen <- thisPlot
+
+library(patchwork)
+cb <- countryVerticalPlot + biosphereVerticalPlot +
+  plot_layout(widths = c(0.9, 1.2))
+cg <- countryVerticalPlot + biosphereVerticalPlotGreen +
+  plot_layout(widths = c(0.9, 1.2))
+
+ggsave("png/country_biosphere_vertical_green.png", plot=cg,
+       device = ragg::agg_png, dpi = 1000,
+       units="in", width=2, height=3.5,
+       scaling = 0.45)
+ggsave("png/biosphere_vertical_blues.png", plot=thisPlot,
+       device = ragg::agg_png, dpi = 1000,
+       units="in", width=1.2, height=3.5,
+       scaling = 0.45)
+
+country_img <- image_read('png/country_vertical_centred.png')
+biosphere_green_img <- image_read('png/biosphere_vertical_greens.png')
+biosphere_blue_img <- image_read('png/biosphere_vertical_blues.png')
+image_info(country_img)
+image_info(biosphere_blue_img)
+
+img2g <- c(country_img, biosphere_green_img)
+cbg <- image_append(image_scale(img2g))
+image_write(cbg, path='png/countriesBiosphereGreen.png', format='png')
+
+img2b <- c(country_img, biosphere_blue_img)
+cbb <- image_append(image_scale(img2b))
+image_write(cbb, path='png/countriesBiosphereBlue.png', format='png')
