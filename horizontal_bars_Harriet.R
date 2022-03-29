e2518899b29bd8835c77b7e246aa1a4549adbb04
jpmckeown <j.p.mckeown@icloud.com>
2022-03-29 11:18:39 +0100

    assembled 3 horizontal bars

--- horizontal_bars_Harriet.R
+++ horizontal_bars_Harriet.R
@@ -25,7 +25,9 @@ show_col(midGreens4)
 
 OrangeLong <- colorRampPalette(brewer.pal(9, 'Oranges'))(15)
 midOranges4 <- OrangeLong[8:5]
+midOranges3 <- OrangeLong[7:5]
 show_col(midOranges4)
+show_col(midOranges3)
 
 PurpleLong <- colorRampPalette(brewer.pal(9, 'Purples'))(16)
 midPurples4 <- PurpleLong[8:5]
@@ -54,7 +56,7 @@ barOnlyTheme <-  theme(
 
 ## Get data ########
 
-original_xls <- "data/Script-friendly P1_SLR data extraction sheet 180222_Combined.xlsx"
+original_xls <- "data/Script-friendly P1_SLR data extraction sheet 180222_Combined (1).xlsx"
 
 column_names <- read_excel(original_xls, 
                            sheet = "Data gathering Syikin", 
@@ -121,18 +123,18 @@ thisPlot <- ggplot(data = df, aes(y = 1, x = Absolute, fill = Name)) +
   geom_text(aes(label = Name),
             alpha = ifelse(df$Name == 'Low', 0, 1),
             position = position_stack(vjust = 0.5), 
-            size = ifelse(df$Name == 'Unclear', 5, 6), 
+            size = 7, 
             vjust = -0.5) +
   geom_text(aes(label = Str),
             alpha = ifelse(df$Name == 'Low', 0, 1),
             position = position_stack(vjust = 0.5), 
-            size = ifelse(df$Name == 'Unclear', 4.5, 5), 
+            size = 6, 
             vjust = 1.5) +
   geom_text(aes(label = paste(Name)),
             alpha = ifelse(df$Name == 'Low', 1, 0),
             angle = 90,
             position = position_stack(vjust = 0.5), 
-            size = 5, 
+            size = 5.5, 
             hjust = 1.5) +
   geom_text(aes(label = paste(Str)),
             alpha = ifelse(df$Name == 'Low', 1, 0),
@@ -146,9 +148,9 @@ thisPlot <- ggplot(data = df, aes(y = 1, x = Absolute, fill = Name)) +
   barOnlyTheme
 
 thisPlot
-validityHorizontalPlot_M <- thisPlot  # ready for stitching
+validityHorizontalPlot_M_larger <- thisPlot  # ready for stitching
 
-ggsave("png/validity_Malaysia.png", plot=thisPlot,
+ggsave("png/validity_Malaysia_font_7.png", plot=thisPlot,
        device = ragg::agg_png, dpi = 1000,
        units="in", width=3, height=0.7,
        scaling = 0.45)
@@ -163,7 +165,7 @@ thisData <- mde %>%
   select(`Publication type`) %>% 
   drop_na()
 
-thisData[thisData == 'Grey literature organisational report',] <- 'Grey literature\norganisational report'
+thisData[thisData == 'Grey literature organisational report',] <- 'Grey literature'
 thisData[thisData == 'Peer-reviewed published literature',] <- 'Peer-reviewed'
 
 thisTable <- tabyl(thisData$`Publication type`)
@@ -173,8 +175,7 @@ names(thisTable)[1] <- 'Data_source'
 # thisTable <- thisTable[order(-thisTable$n),]  
 
 # custom specified order
-# seems to make no difference, Peer-reviewed goes left
-orderSource <- c('Peer-reviewed', 'Grey literature\norganisational report')
+orderSource <- c('Peer-reviewed', 'Grey literature')
 # orderSource <- c('Grey literature\norganisational report', 'Peer-reviewed')
 thisTable <- thisTable %>%
  slice(match(orderSource, Data_source))
@@ -212,31 +213,19 @@ thisPlot <- ggplot(data = df, aes(y = 1, x = Absolute, fill = Name)) +
   geom_col(color = 'black', size = 0.1) +
   geom_text(aes(label = Name),
             position = position_stack(vjust = 0.5), 
-            size = 6, vjust = -0.5) +
+            size = 7, vjust = -0.5) +
   geom_text(aes(label = Str),
             position = position_stack(vjust = 0.5), 
-            size = 5, vjust = 1.5) +
-  # geom_text(aes(label = Name),
-  #           position = position_stack(vjust = 0.5),
-  #           alpha = ifelse(df$vLab, 1, 0),
-  #           size = ifelse(df$Name=='Conf. Abstract', 4, 5),
-  #           hjust = ifelse(df$Name=='Conf. Abstract', 0.8, 1.2),
-  #           angle = 90) +
-  # geom_text(aes(label = Str),
-  #           position = position_stack(vjust = 0.5),
-  #           alpha = ifelse(df$vLab, 1, 0),
-  #           hjust = ifelse(df$Name=='Conf. Abstract', -0.7, -0.3),
-  #           size = ifelse(df$Name=='Conf. Abstract', 4, 4.5),
-  #           angle = 90) +
+            size = 6, vjust = 1.5) +
   scale_x_continuous(limits=c(0, n), expand = c(0, 0)) +
   scale_y_discrete(expand = c(0, 0)) +
   scale_fill_manual(values = midPurples2) +
   barOnlyTheme
 
 thisPlot
-dataSourceHorizontalPlot_Malaysia <- thisPlot  # ready for stitching
+dataSource_Malaysia_larger <- thisPlot  # ready for stitching
 
-ggsave("png/dataSource_Malaysia.png", plot=thisPlot,
+ggsave("png/dataSource_Malaysia_font_7.png", plot=thisPlot,
        device = ragg::agg_png, dpi = 1000,
        units="in", width=3, height=0.8,
        scaling = 0.45)
@@ -269,7 +258,7 @@ thisTable <- tabyl(thisData$Study_design)
 names(thisTable)[1] <- 'Study_design'
 
 # custom order of segments
-col_order <- rev(c('Non-controlled', 'CI', 'BA', 'BACI'))
+col_order <- rev(c('Case study', 'Cross sectional', 'Case control'))
 studyDesignTableT1 <- thisTable %>% 
   slice(match(col_order, Study_design))
 
@@ -280,15 +269,15 @@ Value <- thisTable$percent
 Absolute <- thisTable$n
 percentage <- (round(Value * 100, digits=0))
 Str <- paste0(Absolute, ' (', percentage, '%)')
-wideLab <- rev(c(TRUE, TRUE, FALSE, FALSE))
+wideLab <- rev(c(FALSE, TRUE, FALSE))
 n <- sum(Absolute)
 
 df <-  data.frame(Name, Absolute, Value, Str, wideLab)
 df$Name <- factor(df$Name, levels = df$Name)
 df
 
-T1studyDesign_df <- df  # temporary archive
-df <- T1studyDesign_df
+T1studyDesign_df_H <- df  # temporary archive
+df <- T1studyDesign_df_H
 
 # Plot: alpha 0 hides horizontal labels for BACI segment,
 #  and alpha 1 shows vertical label only for BACI.
@@ -296,39 +285,42 @@ df <- T1studyDesign_df
 thisPlot <- ggplot(data = df, aes(y = 1, x = Absolute, fill = Name)) +
   geom_col(color = 'black', size = 0.1) +
   geom_text(aes(label = Name),
-            position = position_stack(vjust = 0.5), vjust = -0.5,
-            size = ifelse(df$wideLab, 6, 5),
-            alpha = ifelse(df$Name == 'BACI', 0, 1)) +
+            alpha = ifelse(df$wideLab, 1, 0),
+            position = position_stack(vjust = 0.5), 
+            size = 7, vjust = -0.5) +
   geom_text(aes(label = Str),
             position = position_stack(vjust = 0.5), vjust = 1.5,
-            size = ifelse(df$wideLab, 5, 4),
-            alpha = ifelse(df$Name == 'BACI', 0, 1)) +
+            size = 6,
+            alpha = ifelse(df$wideLab, 1, 0)) +
   geom_text(aes(label = Name),
-            position = position_stack(vjust = 0.5),
-            alpha = ifelse(df$Name == 'BACI', 1, 0),
-            size = 4.5, angle = 90, hjust = 1.2) +
+            alpha = ifelse(df$wideLab, 0, 1),
+            position = position_stack(vjust = 0.3),
+            angle = 90, 
+            size = 5.7, hjust = 0.5) +
   geom_text(aes(label = Str),
-            position = position_stack(vjust = 0.5),
-            alpha = ifelse(df$Name == 'BACI', 1, 0),
-            size = 4, angle = 90, hjust = -0.1) +
+            alpha = ifelse(df$wideLab, 0, 1),
+            position = position_stack(vjust = 0.7),
+            angle = 90, 
+            size = 5, hjust = 0.5) +
   scale_x_continuous(limits=c(0, n), expand = c(0, 0)) +
   scale_y_discrete(expand = c(0, 0)) +
-  scale_fill_manual(values = midOranges4) +
+  scale_fill_manual(values = midOranges3) +
   barOnlyTheme
 
 thisPlot
-table1_studyDesignHorizontalPlot <- thisPlot
+table1_studyDesign_Harriet <- thisPlot
 
-ggsave("png/studyDesign_horizontal.png", plot=thisPlot,
+ggsave("png/studyDesign_Harriet.png", plot=thisPlot,
        device = ragg::agg_png, dpi = 1000,
        units="in", width=3, height=0.7,
        scaling = 0.45)
 
 
-#### combine 4 bars ###############
+#### combine 3 bars ###############
 
-vars <- c('Type\nof data', 'Study\ndesign', 
-          'Internal\nvalidity\nrating', 'Source\nof data')
+vars <- c('Study\ndesign', 
+          'Internal\nvalidity\nrating', 
+          'Source\nof data')
 
 label_plot <- function(label) {
   ggplot() + 
@@ -338,18 +330,19 @@ label_plot <- function(label) {
     theme(plot.margin = margin(0,0,0,0,'pt'))
 }
 
-figure_hbars <- label_plot(vars[1]) + typeOfDataHorizontalPlot +
+figure_hbars <- label_plot(vars[1]) + 
+  table1_studyDesign_Harriet + 
   plot_spacer() + plot_spacer() +
-  label_plot(vars[2]) + table1_studyDesignHorizontalPlot + 
+  label_plot(vars[2]) + 
+  validityHorizontalPlot_M_larger +
   plot_spacer() + plot_spacer() +
-  label_plot(vars[3]) + validityHorizontalPlot +
-  plot_spacer() + plot_spacer() +
-  label_plot(vars[4]) + dataSourceHorizontalPlot +
-  plot_layout(nrow = 7, widths = c(323, 3220), 
-              heights = c(700, 30, 700, 30, 700, 30, 840))
+  label_plot(vars[3]) + 
+  dataSource_Malaysia_larger +
+  plot_layout(nrow = 5, widths = c(323, 3220), 
+              heights = c(700, 30, 700, 30, 700))
 figure_hbars
 
-ggsave("png/horizontal_bars.png", plot=figure_hbars,
+ggsave("png/horizontal_bars_Harriet.png", plot=figure_hbars,
        device = ragg::agg_png, dpi = 1000,
        units="in", width=3.453, height=3.03,
        scaling = 0.45)
